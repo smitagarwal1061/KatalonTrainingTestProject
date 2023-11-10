506e1f1d6f6b3a0c55738da33bc1edd95f140c3a
Smit.Agarwal <Smit.Agarwal@LAP-TZD-1X97292.IN.COFORGETECH.COM>
2023-11-10 15:28:31 +0530

    Adding suite for single test case execution

---
+++ Test Suites/DemoTestSuiteSingleTestCase.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DemoTestSuiteSingleTestCase</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>735e46c3-2bb7-4ac0-8250-1c3b8c187f0e</testSuiteGuid>
+   <testCaseLink>
+      <guid>2649e21d-ccb0-4911-86e0-a5847d9dd07f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GuiTestCases/KatalonDemoSite</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d56ed2bd-e281-4706-8e37-25ad31f42c4d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/GuiTestCases/OrangeHrmUi</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6c812ac5-aa5e-4e94-bddd-080556452530</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/GuiTestCases/RahulShettyPraticePageScroll</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>66a566c0-5b59-439a-9e4c-fcdb8cfbc789</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/ApiTestCases/PaypalAuth2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
